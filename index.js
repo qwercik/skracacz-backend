416fd543deafc8b6e4a9205340e2ced002813742
qwercik <erykandrzejewski@gmail.com>
2019-10-25 21:34:34 +0200

    Set up express project

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const logger = require('morgan')
+const indexRouter = require('./routes/index')
+
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-  response.send({
-    message: 'Hello, API world!'
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', indexRouter)
+
+// error handler
+app.use((req, res) => {
+  res.status(404).send({
+    message: 'Unknown route'
   })
 })
 
-app.listen(port, () => console.log(`Webserver running on port ${port}`))
+app.listen(port, () => console.log(`Web server running at port ${port}`))
