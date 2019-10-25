711e8248efc705be932c59be12964f9a45924a48
qwercik <erykandrzejewski@gmail.com>
2019-10-25 21:51:14 +0200

    Install app-module-path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const logger = require('morgan')
-const indexRouter = require('./routes/index')
+const indexRouter = require('routes/index')
 
 const app = express()
 const port = 3000
